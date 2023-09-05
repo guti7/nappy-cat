204f474ed372f95d355229deb865717ee3a24b11
José Gutiérrez <7505855+guti7@users.noreply.github.com>
2023-09-05 12:37:07 -0400

    Give the bed a physical body

--- NappyCat/BedNode.swift
+++ NappyCat/BedNode.swift
@@ -10,5 +10,11 @@ import SpriteKit
 class BedNode: SKSpriteNode, EventListenerNode {
     func didMoveToScene() {
         print("bed added to scene")
+        
+        // give the bed a body for the purpose of detecting contacts
+        let bedBodySize = CGSize(width: 40.0, height: 30.0)
+        physicsBody = SKPhysicsBody(rectangleOf: bedBodySize)
+        // make the body static because you don't want it to move
+        physicsBody!.isDynamic = false
     }
 }
