4204875f0dc8fbb4f6b24aed618493d5d322797a
José Gutiérrez <7505855+guti7@users.noreply.github.com>
2023-09-04 20:51:27 -0400

    Add custom cat node

---
+++ NappyCat/CatNode.swift
@@ -0,0 +1,15 @@
+//
+//  CatNode.swift
+//  NappyCat
+//
+//  Created by peche on 9/4/23.
+//
+
+import SpriteKit
+
+class CatNode: SKSpriteNode, EventListenerNode {
+    
+    func didMoveToScene() {
+        print("cat added to scene")
+    }
+}
