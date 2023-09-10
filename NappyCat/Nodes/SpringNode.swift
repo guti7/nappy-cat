7de79270875e017fd802892ae10bfb8638ff9455
José Gutiérrez <7505855+guti7@users.noreply.github.com>
2023-09-09 21:59:52 -0400

    Add impulse to spring on touch up

--- NappyCat/Nodes/SpringNode.swift
+++ NappyCat/Nodes/SpringNode.swift
@@ -11,10 +11,22 @@ class SpringNode: SKSpriteNode, EventListenerNode, InteractiveNode {
     
     
     func didMoveToScene() {
-        print("Spring added")
+        isUserInteractionEnabled = true
     }
     
     func interact() {
-        print("Spring interaction recorded")
+        isUserInteractionEnabled = false
+        physicsBody!.applyImpulse(CGVector(dx: 0, dy: 250), at: CGPoint(x: size.width / 2, y: size.height))
+        
+        run(SKAction.sequence([
+            SKAction.wait(forDuration: 1),
+            SKAction.removeFromParent()
+            ]))
+    }
+    
+    // Respond to taps on the block
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesEnded(touches, with: event)
+        interact()
     }
 }
