4b305d4ee1f08a23e7e363ecdfd11cb070bc3240
José Gutiérrez <7505855+guti7@users.noreply.github.com>
2023-09-18 17:00:45 -0400

    Put a picture in the picture frame using Crop Node

--- NappyCat/Nodes/PictureNode.swift
+++ NappyCat/Nodes/PictureNode.swift
@@ -11,6 +11,17 @@ class PictureNode: SKSpriteNode, EventListenerNode, InteractiveNode {
     
     func didMoveToScene() {
         // enables touches on the picture node
+        isUserInteractionEnabled = true
+        
+        // loads images into sprite nodes
+        let pictureNode = SKSpriteNode(imageNamed: "picture")
+        let maskNode = SKSpriteNode(imageNamed: "picture-frame-mask")
+        
+        // add the crop node
+        let cropNode = SKCropNode()
+        cropNode.addChild(pictureNode)
+        cropNode.maskNode = maskNode
+        addChild(cropNode)
     }
     
     func interact() {
