c265b935927828e76a431f2f903e8dd89001112c
José Gutiérrez <7505855+guti7@users.noreply.github.com>
2023-09-18 21:05:25 -0400

    Play disco lights from the disco ball upon scene start

--- NappyCat/Nodes/DiscoBallNode.swift
+++ NappyCat/Nodes/DiscoBallNode.swift
@@ -6,7 +6,31 @@
 //
 
 import SpriteKit
+import AVFoundation
 
-class DiscoBallNode: SKSpriteNode {
+class DiscoBallNode: SKSpriteNode, EventListenerNode, InteractiveNode {
     
+    private var player: AVPlayer!
+    private var video: SKVideoNode!
+
+    func didMoveToScene() {
+        
+        // load video file
+        let fileURL = Bundle.main.url(forResource: "discolights-loop", withExtension: "mov")!
+        player = AVPlayer(url: fileURL)
+        video = SKVideoNode(avPlayer: player)
+        
+        // position video in scene frame
+        video.size = scene!.size
+        video.position = CGPoint(x: scene!.frame.midX, y: scene!.frame.midY)
+        video.zPosition = -1
+        scene!.addChild(video)
+        
+        // play the video
+        video.play()
+    }
+    
+    func interact() {
+        // TODO: Interact with disco ball node, play video, play music?
+    }
 }
