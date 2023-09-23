e140ecb08f8206b242d648b4f31331cb5d991422
José Gutiérrez <7505855+guti7@users.noreply.github.com>
2023-09-22 22:20:58 -0400

    Add in arrow shape for hint node

--- NappyCat/Nodes/HintNode.swift
+++ NappyCat/Nodes/HintNode.swift
@@ -9,7 +9,31 @@ import SpriteKit
 
 class HintNode: SKSpriteNode, EventListenerNode {
     
+    /// The path for the arrow hint shape node
+    var arrowPath: CGPath = {
+        let bezierPath = UIBezierPath()
+        bezierPath.move(to: CGPoint(x: 0.5, y: 65.69))
+        bezierPath.addLine(to: CGPoint(x: 74.99, y: 1.5))
+        bezierPath.addLine(to: CGPoint(x: 74.99, y: 38.66))
+        bezierPath.addLine(to: CGPoint(x: 257.5, y: 38.66))
+        bezierPath.addLine(to: CGPoint(x: 257.5, y: 92.72))
+        bezierPath.addLine(to: CGPoint(x: 74.99, y: 92.72))
+        bezierPath.addLine(to: CGPoint(x: 74.99, y: 126.5))
+        bezierPath.addLine(to: CGPoint(x: 0.5, y: 65.69))
+        bezierPath.close()
+        
+        return bezierPath.cgPath
+    }()
+    
     func didMoveToScene() {
         color = SKColor.clear
+        
+        // Create a shape node from arrow path
+        let shape = SKShapeNode(path: arrowPath)
+        shape.strokeColor = SKColor.gray
+        shape.lineWidth = 4
+        shape.fillColor = SKColor.white
+        
+        addChild(shape)
     }
 }
